670b79a4b40992db0f2175073304257b935a3231
alexbgw <49972373+alexbgw@users.noreply.github.com>
2020-04-26 23:47:11 +0800

    git init space-swiper
    git init space-swiper

---
+++ space_swiper/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="content">
+		<space-swiper></space-swiper>
+	</view>
+</template>
+
+<script>
+	import spaceSwiper from '../../components/space-swiper.vue'
+	export default{
+		components:{
+			spaceSwiper
+		}
+	}
+</script>
+
+<style>
+	
+</style>
